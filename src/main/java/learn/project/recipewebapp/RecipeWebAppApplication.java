44df687da03d3735ff8701feb331c4e1068e52da
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-12-06 19:12:12 +0300

    Домашняя работа 6 декабря - WEB Recipe

--- src/main/java/learn/project/recipewebapp/RecipeWebAppApplication.java
+++ src/main/java/learn/project/recipewebapp/RecipeWebAppApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class RecipeWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeWebAppApplication.class, args);
     }
 
+    //@Bean
+    //public Docket productApi() {
+        //return new Docket(DocumentationType.SWAGGER_2).select()
+        //        .apis(RequestHandlerSelectors.basePackage("learn.project.recipewebapp")).build();
+    //}
 }
